0f32672b337c572783b81de1e355152b18c87770
William Tange <will@tan.ge>
2024-05-06 12:35:15 +0200

    still broken, try to clean up

--- llvm/tools/llvm-json/llvm-json.cpp
+++ llvm/tools/llvm-json/llvm-json.cpp
@@ -47,31 +47,24 @@ EXTERN EMSCRIPTEN_KEEPALIVE int printarray(char *Data, int len) {
   return (int)Data + len;
 }
 
-EXTERN EMSCRIPTEN_KEEPALIVE char readbyte(char *Data) { return *Data; }
-
 EXTERN EMSCRIPTEN_KEEPALIVE LLVMModuleRef *Mod = nullptr;
 
 EXTERN EMSCRIPTEN_KEEPALIVE LLVMModuleRef parse(char *Data) {
-  int imod = (int)*Mod;
-  /* for (int i = 0; i < 16; i++) { */
-  /**/
-  /*   emscripten_log(EM_LOG_INFO, "Mod[%i] = %i", i, *(uint8_t *)(imod + i));
-   */
-  /* } */
   LLVMContext Context;
   LLVMContextRef ContextRef = (LLVMContextRef)&Context;
-  emscripten_log(EM_LOG_INFO, "parse(%p, %p)", Data, Mod);
-  emscripten_log(EM_LOG_INFO, "parse(\"%s\", %x)", Data, *Mod);
+
+  emscripten_log(EM_LOG_INFO, "parse(%p), Mod = %p", Data, Mod);
+  emscripten_log(EM_LOG_INFO, "parse(\"%s\")", Data);
+
   LLVMMemoryBufferRef Buf =
       LLVMCreateMemoryBufferWithMemoryRange(Data, strlen(Data), "", 1);
   LLVMContextRef context_ref;
   LLVMParseIRInContext(ContextRef, Buf, Mod, nullptr);
-  emscripten_log(EM_LOG_INFO, "parse(%p, %p)", Data, Mod);
-  emscripten_log(EM_LOG_INFO, "parse(\"%s\", %x)", Data, *Mod);
-  emscripten_log(EM_LOG_INFO, "0 %#x", Data, *((uint8_t *)*Mod));
-  emscripten_log(EM_LOG_INFO, "%p", ((uint8_t *)*Mod));
-  // emscripten_log(EM_LOG_INFO, "%#x", Data, *(&(((uint8_t *)*Mod)[0])));
-  auto ptr = (uint8_t *)*Mod;
+
+  emscripten_log(EM_LOG_INFO, "parse(%p), Mod = %p", Data, Mod);
+  emscripten_log(EM_LOG_INFO, "parse(\"%s\"), *Mod = %x", Data, *Mod);
+
+  /*auto ptr = (uint8_t *)*Mod;
   for (int i = 0; i < 16; i++) {
 
     emscripten_log(EM_LOG_INFO, "Ptr[%i] = %i", i, *(uint8_t *)(ptr + i));
@@ -80,7 +73,7 @@ EXTERN EMSCRIPTEN_KEEPALIVE LLVMModuleRef parse(char *Data) {
   for (int i = 0; i < 16; i++) {
 
     emscripten_log(EM_LOG_INFO, "Mod[%i] = %i", i, *(uint8_t *)(imod + i));
-  }
+  }*/
   //*((char *)*Mod) = 69;
   // emscripten_log(EM_LOG_INFO, "0 %#x", Data, ((uint8_t *)(((int)*Mod) + 0)));
   // emscripten_log(EM_LOG_INFO, "1 %#x", Data, ((uint8_t *)(((int)*Mod) + 1)));
